99171b625ba903c3e845132d050107887856e1bc
Pabllo Cristian <pabllo.dev@gmail.com>
2024-08-24 10:52:40 -0300

    Update triTypes.js

--- functions/triTypes.js
+++ functions/triTypes.js
@@ -1,11 +1,19 @@
-function triTypes(a,b,c) {
-    return (a + b > c && a + c > b && b + c > a) ?
-        (a === b && a === c)
-            ? 'equilateral' :
-            (a === b || a === c || b === c)
-                ? 'isosceles'
-                : 'scalene'
-        : 'none'
+function triangleTypes(a, b, c) {
+    //is triangle
+    if( a + b > c && a + c > b && b + c > a){
+        //all equal = equilateral
+        if(a === b && a === c && b === c){
+            return 'equilateral';
+
+        //all different = scalene
+        } else if(a != b && a != c) {
+            return 'scalene';
+        }
+        //two equal, one different
+        return 'isosceles'
+    }
+    //isnt triangle
+    return 'none';
 }
 
-module.export = triTypes;
+module.exports = triangleTypes;
